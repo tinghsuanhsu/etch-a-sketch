28a56a6de8b063aa47ff878021f53f62ca6ca471
tinghsuanhsu <tinghsuanhsu@gmail.com>
2023-08-31 19:52:25 +1000

    Add functions to generate random colour and draw random colour

--- main.js
+++ main.js
@@ -1,93 +1,136 @@
-// logic
-// create a container in html to host the sketch
-// 1. dynamically create the box with js
-// 2. add interaction to mouse hover
 
-const container = document.querySelector('.grid-container');
-let colour = 'blue'
-let size = 15
+// initialise variables
+const gridContainer = document.querySelector('.grid-container');
+let colour = 'blue';
+let size = 15;
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
 
+// function to make rows
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    let gridID = c + 1
-    cell.setAttribute('id', `grid-${gridID}`)
-    container.appendChild(cell).className = "grid-item";
+    let gridID = c + 1;
+    cell.setAttribute('id', `grid-${gridID}`);
+    gridContainer.appendChild(cell).className = "grid-item";
   };
 };
 
+function removeChildFromParent(element){
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    };   
+};
 
+function generateRandomColour(){
+    let hexColour = '#'
+    for (let i = 0; i < 6; i++) {
+        const randomPosition = Math.floor(Math.random() * hexCharacters.length)
+        hexColour += randomPosition
+    }
+    return hexColour
+} 
 
-function setGridItemColour() {
-    console.log('hi')
-}
-
-function changeGridSize() {
-    const gridSize = document.querySelector('#gridSize')
+// function to reset grid and create a new blank grid
+function resetGrid() {
+   
+    const popUp = document.getElementById('popup');
+    popUp.style.display = 'block';
+    
+    
+    const gridSize = document.getElementById('resetGridSize');
     gridSize.addEventListener('change', function(e) {
         size = e.target.value;
+    
     })
+
+    const applyGridSize = document.getElementById('apply-gird-change-btn');
+    applyGridSize.addEventListener('click', function(){
+        popUp.style.display = 'none';
+    });
+
+    removeChildFromParent(gridContainer);
+    makeRows(size, size);
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.style.background = 'white');
+    sketch();
 }
 
-function sketch() {
-    const gridItems = document.querySelectorAll('.grid-item')
-    gridItems.forEach(item => item.addEventListener('mouseover', function(e) {
-        const sketch = document.getElementById(e.target.id)
 
-        sketch.style.background = colour
-        console.log(colour)
+// function to sketch 
+function sketch() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.addEventListener('mouseover', function(e) {
+        const sketch = document.getElementById(e.target.id);
+        sketch.style.background = colour;
     }))
 }
 
+// function to change colour according to user input
 function changeColour() {
-    const colorSelected = document.querySelector('#colorSelected')
+    const colorSelected = document.getElementById('colorSelected');
     colorSelected.addEventListener('change', function(e) {
-        colour = e.target.value
+        colour = e.target.value;
     })
     
 }
 
+// function to clear the sketch
 function clearSketch() {
-    const girdItems = document.getElementsByClassName('grid-item')
+    const girdItems = document.getElementsByClassName('grid-item');
     for (let i=0; i < girdItems.length; i++) {
-        girdItems[i].style.background = 'white'
+        girdItems[i].style.background = 'white';
     }
 }
 
+// function to erase the drawing one cell by one cell 
 function eraseSketch() {
-    const gridItems = document.querySelectorAll('.grid-item')
+    const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(item => item.addEventListener('mouseover', function(e) {
-        const sketch = document.getElementById(e.target.id)
-        sketch.style.background = 'white'
+        const sketch = document.getElementById(e.target.id);
+        sketch.style.background = 'white';
     }))
 }
 
 
-// let root = document.documentElement;
-// console.log(colorSelected)
+function drawRandomColour() {
+    let randomColour = generateRandomColour()
+    colour = randomColour
+}
+
+// how the sketch should work
+function etchASketch(){
+    makeRows(size, size);    
+    sketch();
+    const clearButton = document.getElementById('clear-btn');
+    clearButton.addEventListener('click', clearSketch);
+
+    const eraserButton = document.getElementById('eraser-btn');
+    eraserButton.addEventListener('click', eraseSketch);
+
+    const changeButton = document.getElementById('color-picker-btn');
+    changeButton.addEventListener('click', changeColour);
+
+    const gridSizeButton = document.getElementById('apply-gird-change-btn');
+    gridSizeButton.addEventListener('click', resetGrid);
+
+    const drawButton = document.getElementById('sketch-btn');
+    drawButton.addEventListener('click', sketch);
+
+    const resetButton = document.getElementById('reset-btn');
+    resetButton.addEventListener('click', resetGrid);
+
+    const randomColourButton = document.getElementById('random-colour-btn');
+    randomColourButton.addEventListener('click', drawRandomColour);
+
+    
+
+}
 
 
-// TODO 
-// reset the colour 
 
 
-makeRows(size, size);
-sketch()
-const clearButton = document.querySelector('#clear-btn')
-clearButton.addEventListener('click', clearSketch)
 
-const eraserButton = document.querySelector('#eraser-btn')
-eraserButton.addEventListener('click', eraseSketch)
-
-const changeButton = document.querySelector('#color-picker-btn')
-changeButton.addEventListener('click', changeColour)
-
-const drawButton = document.querySelector('#sketch-btn')
-drawButton.addEventListener('click', sketch)
-
-console.log(colour)
-// make makeRows dynamic
-// make background colour dynamic
+etchASketch()
