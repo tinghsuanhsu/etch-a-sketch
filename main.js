77940806c6663ff8e4a61dd492aa1ea246c877f6
tinghsuanhsu <tinghsuanhsu@gmail.com>
2023-09-08 08:47:36 +1000

    Update drawing logic to use random colour or current colour in the mouseover event listener based on the active button selected

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
 // initialise variables
 
-let colour = 'blue';
+let currentColour = 'blue';
 let size = 15;
 let activeButton;
 const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-
-
-
 const defaultColour = '#000000';
 const eraserColour = '#FFFFFF';
+
 const gridContainer = document.querySelector('.grid-container');
 const clearButton = document.getElementById('clear-btn');
 const eraserButton = document.getElementById('eraser-btn');
@@ -17,6 +15,7 @@ const randomColourButton = document.getElementById('random-colour-btn');
 const changeColourButton = document.getElementById('colorSelected');
 const gridSizeButton = document.getElementById('apply-gird-change-btn');
 const resetButton = document.getElementById('reset-btn');
+let gridItems = document.getElementsByClassName('grid-item');
 
 
 createGrid(size, size);   
@@ -25,11 +24,7 @@ clearButton.addEventListener('click', clearSketch);
 eraserButton.addEventListener('click', setActiveButtonAndColour);
 randomColourButton.addEventListener('click', setActiveButtonAndColour);
 changeColourButton.addEventListener('change', setActiveButtonAndColour);
-resetButton.addEventListener('click', resetGrid);
-
-// etchASketch()
- 
-
+resetButton.addEventListener('click', resetGrid );
 
 
 function setColour(){
@@ -39,9 +34,9 @@ function setColour(){
         case gridSizeButton.id:
             return defaultColour
         case changeColourButton.id:
-            return changeColour();
+            return changeColour()
         case randomColourButton.id:
-            return drawRandomColour()
+            return generateRandomColour()
         case eraserButton.id:
             return eraserColour;
         case clearButton.id:
@@ -51,9 +46,9 @@ function setColour(){
 }
 
 function setActiveButtonAndColour(){
-    activeButton = this.id
-    colour = setColour()
-    console.log(this.id)
+    activeButton = this.id;
+    currentColour = setColour();
+
 }
 
 // function to make rows
@@ -61,14 +56,15 @@ function createGrid(rows, cols) {
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement('div');
-    let gridID = c + 1;
-    cell.setAttribute('id', `grid-${gridID}`);
-    cell.style.background = 'white';
-    cell.className = 'grid-item';
-    gridContainer.appendChild(cell)
-    // gridItems.forEach(item => item.style.background = 'white');
+        let cell = document.createElement('div');
+        let gridID = c + 1;
+        cell.setAttribute('id', `grid-${gridID}`);
+        cell.style.background = 'white';
+        cell.className = 'grid-item';
+        gridContainer.appendChild(cell);
   };
+    gridItems = document.getElementsByClassName('grid-item');
+
 };
 
 function removeChildFromParent(element){
@@ -81,7 +77,7 @@ function generateRandomColour(){
     let hexColour = '#'
     for (let i = 0; i < 6; i++) {
         const randomPosition = Math.floor(Math.random() * hexCharacters.length)
-        hexColour += hexCharacters[randomPosition]
+        hexColour += hexCharacters[randomPosition];
     }
     return hexColour
 } 
@@ -89,17 +85,19 @@ function generateRandomColour(){
 
 // function to change colour according to user input
 function changeColour() {
+
+    activeButton = this.id;
     const colorSelected = document.getElementById('colorSelected');
     colorSelected.addEventListener('change', function(e) {
-        colour = e.target.value;
+        currentColour = e.target.value;
     })
-    return colour
+    return currentColour
 }
 
 // function to reset grid and create a new blank grid
 function resetGrid() {
-   
-    colour = defaultColour
+    activeButton = this.id;
+    currentColour = defaultColour;
     const popUp = document.getElementById('popup');
     popUp.style.display = 'block';
     
@@ -113,86 +111,25 @@ function resetGrid() {
         popUp.style.display = 'none';
         removeChildFromParent(gridContainer);
         createGrid(size, size);
-        gridItems = document.getElementsByClassName('grid-item');
         sketch();
     });
     
     
 }
 
-
-// function to sketch 
 function sketch() {
-    const gridItems = document.getElementsByClassName('grid-item');
     [...gridItems].forEach(item => item.addEventListener('mouseover', function() {
-        item.style.background = colour;
+        if (activeButton === randomColourButton.id) {
+            item.style.background = generateRandomColour();
+        } else { item.style.background = currentColour; }
         })
     )
-}    
-// function to clear the sketch
+}
+
 function clearSketch() {
+    activeButton = this.id;
     const girdItems = document.getElementsByClassName('grid-item');
     for (let i=0; i < girdItems.length; i++) {
         girdItems[i].style.background = 'white';
     }
 }
-
-
-function deactiveButton() {
-    isActiveButton = false;
-}
-
-function drawRandomColour() {
-    if (activeButton === randomColourButton.id){
-        const gridItems = document.getElementsByClassName('grid-item');
-        [...gridItems].forEach(item => item.addEventListener('mouseover', function(e) {
-            let randomColour = generateRandomColour()
-            colour = randomColour
-        }, {once:true}))
-        return colour
-    } 
-}
-
-// function drawRandomColour() {
-//     const gridItems = document.getElementsByClassName('grid-item');
-//     [...gridItems].forEach(item => item.addEventListener('mouseover', function(e) {
-//         let randomColour = generateRandomColour()
-//         colour = randomColour
-//         item.style.background = colour;
-//     }))
-//     console.log(`drawrandome ${colour}`)
-// }
-
-// how the sketch should work
-function etchASketch(){
-
-    
-    const clearButton = document.getElementById('clear-btn');
-    clearButton.addEventListener('click', clearSketch);
-
-    const eraserButton = document.getElementById('eraser-btn');
-    eraserButton.addEventListener('click', eraseSketch);
-
-    const randomColourButton = document.getElementById('random-colour-btn');
-    randomColourButton.addEventListener('click', drawRandomColour);
-
-    const changeButton = document.getElementById('colorSelected');
-    changeButton.addEventListener('click', changeColour);
-
-    const gridSizeButton = document.getElementById('apply-gird-change-btn');
-    gridSizeButton.addEventListener('click', resetGrid);
-
-    const drawButton = document.getElementById('sketch-btn');
-    drawButton.addEventListener('click', sketch);
-
-    const resetButton = document.getElementById('reset-btn');
-    resetButton.addEventListener('click', resetGrid);
-
-
-}
-
-
-
-
-
-console.log(activeButton)
